c7767f81158fbd60e2a9774a88df9e5bc78fdf4a
Chris Getz <getzboxing@gmail.com>
2015-10-15 15:39:10 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//1.	Add a button to a web page. Use the old school HTML attribute method to attach a click event handler to the button. That handler should call a function that displays the message “I have been clicked” out into an alert box.
+
+function myFunction() {
+    
+    window.alert("I have been clicked!");
+}
+
+//2.	Add a button to a web page and uniquely ID the button. Write a script that finds the button by its ID. Then use the DOM Element method to associate a function with the onclick event of the button. Again, display the message “I have been clicked” out into an alert box.
+
+    
+
+//3.	Add a button to a web page and uniquely ID the button. Write a script that attaches an event listener to the button and listens for the click event. When it’s heard, a user-defined function should be called and the message “I have been clicked” should be displayed out into an alert box.
